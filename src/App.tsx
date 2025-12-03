237af20114e6e7009af3f343ae30ab6f8c2b8553
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2025-12-03 10:40:36 +0100

    create type and TodoItem component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Form from "./components/TodoForm";
-
-type Todo = {
-  id: number;
-  text: string;
-  completed: boolean;
-};
+import TodoItem from "./components/TodoItem";
+import type { Todo } from "./types";
 
 function App() {
   const initialState = () => {
@@ -29,11 +25,11 @@ function App() {
     setTodos([...todos, todo]);
   };
 
-  const handleClick = (id: Todo["id"]) => {
+  const onDelete = (id: Todo["id"]) => {
     setTodos(todos.filter((tarea) => tarea.id !== id));
   };
 
-  const toggleTodo = (id: Todo["id"]) => {
+  const onToggle = (id: Todo["id"]) => {
     setTodos(
       todos.map((todo) => {
         if (todo.id === id) {
@@ -56,47 +52,14 @@ function App() {
             <p>Completadas: {todos.filter((todo) => todo.completed).length}</p>
             <p>Pendientes: {todos.filter((todo) => !todo.completed).length}</p>
           </div>
-
           {/* Seccion tareas */}
           {todos.map((todo) => (
-            <div
+            <TodoItem
               key={todo.id}
-              className="flex justify-between place-items-center bg-white shadow-lg rounded-lg p-5"
-            >
-              <button onClick={() => toggleTodo(todo.id)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill={todo.completed ? "#3AE019" : "#BCBFBB"}
-                >
-                  <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-                </svg>
-              </button>
-
-              <p
-                className={`mr-auto p-3 ${
-                  todo.completed ? "line-through text-gray-400" : ""
-                } `}
-              >
-                {todo.text}
-              </p>
-              <button
-                onClick={() => handleClick(todo.id)}
-                className=" text-red-500 p-2 uppercase font-bold"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="30px"
-                  viewBox="0 -960 960 960"
-                  width="30px"
-                  fill="#E80C0C"
-                >
-                  <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-                </svg>
-              </button>
-            </div>
+              todo={todo}
+              onToggle={onToggle}
+              onDelete={onDelete}
+            />
           ))}
         </main>
         <Footer />
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,52 @@
+import type { Todo } from "../types";
+
+type TodoProps = {
+  todo: Todo;
+  onToggle: (id: Todo["id"]) => void;
+  onDelete: (id: Todo["id"]) => void;
+};
+
+const TodoItem = ({ todo, onToggle, onDelete }: TodoProps) => {
+  return (
+    <div
+      key={todo.id}
+      className="flex justify-between place-items-center bg-white shadow-lg rounded-lg p-5"
+    >
+      <button onClick={() => onToggle(todo.id)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill={todo.completed ? "#3AE019" : "#BCBFBB"}
+        >
+          <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+        </svg>
+      </button>
+
+      <p
+        className={`mr-auto p-3 ${
+          todo.completed ? "line-through text-gray-400" : ""
+        } `}
+      >
+        {todo.text}
+      </p>
+      <button
+        onClick={() => onDelete(todo.id)}
+        className=" text-red-500 p-2 uppercase font-bold"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          viewBox="0 -960 960 960"
+          width="30px"
+          fill="#E80C0C"
+        >
+          <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
